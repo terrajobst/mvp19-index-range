9244bdfebf05daaefbae8a974905095a648cc6fd
Immo Landwerth <immol@microsoft.com>
2019-03-20 10:09:19 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace index_and_range
+{
+    class Program
+    {
+        static void Main()
+        {
+            var text = "Hello World";
+            var hello = text.Substring(0, 5);
+            var world = text.Substring(6, 5);
+
+            Console.WriteLine(text);
+            Console.WriteLine(hello);
+            Console.WriteLine(world);
+        }
+    }
+}
